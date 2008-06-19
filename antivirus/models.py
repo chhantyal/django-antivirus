fa9bbc642705ead1cb03b98df2dbbab773359bee
marinho <marinho@e4fb747a-0850-0410-b00d-59b4185cecd8>
2008-06-19 17:53:32 +0000

    first version

---
+++ antivirus/models.py
@@ -0,0 +1,83 @@
+import os
+
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.generic import GenericForeignKey
+from django.utils.translation import ugettext as _
+
+import app_settings
+
+FILE_STANDING = 0
+FILE_VIRUS_NOT_FOUND = 1
+FILE_VIRUS_FOUND = -1
+FILE_NOT_EXISTS = -2
+
+FILE_STATUS_CHOICES = (
+    (FILE_STANDING,        _('Standing')),
+    (FILE_VIRUS_NOT_FOUND, _('Virus not found')),
+    (FILE_VIRUS_FOUND,     _('Virus found')),
+    (FILE_NOT_EXISTS,      _('Not exists')),
+)
+
+class FileManager(models.Manager):
+    def get_or_create_for_object(self, obj, file_path, url=None):
+        ctype = ContentType.objects.get_for_model(obj)
+        file, new = self.get_or_create(
+                path = file_path,
+                defaults = {
+                    'content_type': ctype,
+                    'object_id': obj.id,
+                    'url': url,
+                    }
+                )
+
+        return file
+
+class File(models.Model):
+    path = models.FilePathField(path=app_settings.ANTIVIRUS_FILES_PATH, unique=True, max_length=250)
+    status = models.SmallIntegerField(default=0, blank=True, choices=FILE_STATUS_CHOICES)
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    object = GenericForeignKey()
+    viruses_found = models.TextField(blank=True)
+    url = models.URLField(blank=True, null=True)
+
+    objects = FileManager()
+
+    def __unicode__(self):
+        return self.path
+
+    class Meta:
+        pass
+
+    def scanfile(self):
+        if not self.check_file_exists():
+            return False
+
+        import pyclamav
+        found, virus = pyclamav.scanfile(self.path)
+
+        if found:
+            self.viruses_found = virus
+            self.status = FILE_VIRUS_FOUND
+            self.save()
+        else:
+            self.viruses_found = ''
+            self.status = FILE_VIRUS_NOT_FOUND
+            self.save()
+
+        return found
+
+    def check_file_exists(self):
+        if self.status != FILE_NOT_EXISTS and not os.path.isfile(self.path):
+            self.status = FILE_NOT_EXISTS
+            self.save()
+            return False
+
+        elif self.status == FILE_NOT_EXISTS and os.path.isfile(self.path):
+            self.status = FILE_STANDING
+            self.save()
+
+        return True
+
