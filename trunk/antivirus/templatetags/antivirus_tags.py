ab1b428dd5887c5e24f087d97f80bfdf4474d3bd
marinho <marinho@e4fb747a-0850-0410-b00d-59b4185cecd8>
2009-05-13 12:00:29 +0000

    Adding a template tag "check_virus" to do the same that the template filter "check_for_virus" does

--- trunk/antivirus/templatetags/antivirus_tags.py
+++ trunk/antivirus/templatetags/antivirus_tags.py
@@ -10,19 +10,12 @@ from antivirus import app_settings
 
 register = template.Library()
 
-@register.filter_function
-def check_for_virus(object, field_name):
-    """Uses this template filter to print the HTML for download the file just after check for virus. If a virus be found, it won't show the link to download.
-    
-    Example:
-        
-    {{ object|check_for_virus:"file_field_name" }}
-    """
-    file_field = getattr(object, field_name)
+def generic_check_virus(obj, field_name, url=None):
+    file_field = getattr(obj, field_name)
     file_path = file_field.path
-    url = file_field.url
+    url = url or file_field.url
 
-    file = File.objects.get_or_create_for_object(object, file_path, url)
+    file = File.objects.get_or_create_for_object(obj, file_path, url)
 
     if file.status == FILE_VIRUS_FOUND:
         return app_settings.ANTIVIRUS_MSG_VIRUS_FOUND
@@ -42,6 +35,55 @@ def check_for_virus(object, field_name):
                 'fid': file.id,
                 })
     elif file.status >= FILE_VIRUS_NOT_FOUND:
-        file_field = getattr(object, field_name)
-        return mark_safe('<a href="%s">%s</a>'%(file_field.url, app_settings.ANTIVIRUS_MSG_DOWNLOAD))
+        file_field = getattr(obj, field_name)
+        return mark_safe('<a href="%s">%s</a>'%(
+            url,
+            app_settings.ANTIVIRUS_MSG_DOWNLOAD,
+            ))
+
+@register.filter_function
+def check_for_virus(obj, field_name):
+    """Uses this template filter to print the HTML for download the file just
+    after check for virus. If a virus be found, it won't show the link to download.
+    
+    Example:
+        
+    {{ object|check_for_virus:"file_field_name" }}
+    """
+    return generic_check_virus(obj, field_name)
+
+class CheckVirusRender(template.Node):
+    vobj = None
+    vattr = None
+    vurl = None
+
+    def __init__(self, obj, attr, url=None):
+        self.vobj = template.Variable(obj)
+        self.vattr = template.Variable(attr)
+        self.vurl = template.Variable(url)
+
+    def render(self, context):
+        obj = self.vobj.resolve(context)
+        attr = self.vattr.resolve(context)
+        url = self.vurl.resolve(context)
+
+        ret = generic_check_virus(obj, attr, url)
+
+        return ret
+
+def do_check_virus(parser, token):
+    try:
+        parts = token.split_contents()
+
+        obj = parts[1]
+        attr = parts[2]
+        
+        url = len(parts) > 3 and parts[3] or None
+    except IndexError, e:
+        raise template.TemplateSyntaxError, "%s requires 2 or 3 arguments (object, attribute name and url - optionally)" \
+                % token.contents.split()[0]
+
+    return CheckVirusRender(obj, attr, url)
+
+register.tag('check_virus', do_check_virus)
 
